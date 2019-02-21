26b5270eec6b7c79512f314cf08ba783e70b7df5
DESKTOP-SAVNOE0\KJY <rmarkd32145@naver.com>
2019-02-21 22:54:42 +0900

    tost editText when search button clicked

--- Sky/app/src/main/java/com/example/kjy/sky/MainActivity.kt
+++ Sky/app/src/main/java/com/example/kjy/sky/MainActivity.kt
@@ -2,37 +2,37 @@ package com.example.kjy.sky
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
-    var locationText : String? = null
-
+    private var locationText : String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val locationEditText = findViewById<EditText>(R.id.searchLocation)
         // get reference to button
-        val searchButton = findViewById(R.id.imageButton) as ImageButton
+        val searchButton = findViewById<ImageButton>(R.id.imageButton)
         // set on-click listener
         searchButton.setOnClickListener {
             // your code to perform when the user clicks on the button
-            Toast.makeText(this@MainActivity, "search loaction text", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this@MainActivity,  locationEditText.text, Toast.LENGTH_SHORT).show()
         }
         setUI()
         runAPI()
     }
-
     fun setUI(){
-        val myLocationTextView = findViewById(R.id.myLocationTV) as TextView
+        val myLocationTextView = findViewById<TextView>(R.id.myLocationTV)
         locationText = myLocationTextView.text.toString()
-        println(locationText)
     }
     fun runAPI(){
         val thread = Thread(Runnable {
             try {
-                val result = URL("http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getMsrstnList?serviceKey=VdoZimlDEGPEiSKQ2QP7EwcpiCxTYzGZbh%2BJx6VJTp9Sgpd2pOVcIu2oNdjA%3D%3D&numOfRows=10&pageNo=1&addr=%EC%84%9C%EC%9A%B8&stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC").readText()
+                val result = URL("http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getMsrstnList?serviceKey=VdoZimlDEJ3bJ3X1XwVKPI9Y2wjrxojv9mYGPEiSKQ2QP7EwcpiCxTYzGZbh%2BJx6VJTp9Sgpd2pOVcIu2oNdjA%3D%3D&pageNo=1&addr=%EC%84%9C%EC%9A%B8&stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC").readText()
                 println(result)
             } catch (e: Exception) {
                 e.printStackTrace()
