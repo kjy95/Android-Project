d1b4ef9bc0ca9c21a6057f29eea2f66d13f14e25
DESKTOP-SAVNOE0\KJY <rmarkd32145@naver.com>
2019-02-22 03:31:10 +0900

    get info about location when you search your undName

--- Sky/app/src/main/java/com/example/kjy/sky/MainActivity.kt
+++ Sky/app/src/main/java/com/example/kjy/sky/MainActivity.kt
@@ -13,7 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val locationEditText = findViewById<EditText>(R.id.searchLocation)
         // get reference to button
         val searchButton = findViewById<ImageButton>(R.id.imageButton)
@@ -21,18 +20,18 @@ class MainActivity : AppCompatActivity() {
         searchButton.setOnClickListener {
             // your code to perform when the user clicks on the button
             Toast.makeText(this@MainActivity,  locationEditText.text, Toast.LENGTH_SHORT).show()
+            runAPI(undName = locationEditText.text.toString())
         }
         setUI()
-        runAPI()
     }
     fun setUI(){
         val myLocationTextView = findViewById<TextView>(R.id.myLocationTV)
         locationText = myLocationTextView.text.toString()
     }
-    fun runAPI(){
+    fun runAPI(undName : String){
         val thread = Thread(Runnable {
             try {
-                val result = URL("http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getMsrstnList?serviceKey=VdoZimlDEJ3bJ3X1XwVKPI9Y2wjrxojv9mYGPEiSKQ2QP7EwcpiCxTYzGZbh%2BJx6VJTp9Sgpd2pOVcIu2oNdjA%3D%3D&pageNo=1&addr=%EC%84%9C%EC%9A%B8&stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC").readText()
+                val result = URL(" http://openapi.airkorea.or.kr/openapi/services/rest/MsrstnInfoInqireSvc/getTMStdrCrdnt?serviceKey=VdoZimlDEJ3bJ3X1XwVKPI9Y2wjrxojv9mYGPEiSKQ2QP7EwcpiCxTYzGZbh%2BJx6VJTp9Sgpd2pOVcIu2oNdjA%3D%3D&umdName=".plus(undName)).readText()
                 println(result)
             } catch (e: Exception) {
                 e.printStackTrace()
