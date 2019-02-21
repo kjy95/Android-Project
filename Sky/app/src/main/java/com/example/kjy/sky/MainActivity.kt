c43f1b13a7606cded228c17b1792a3fca4246449
DESKTOP-SAVNOE0\KJY <rmarkd32145@naver.com>
2019-02-21 22:08:53 +0900

    add button listener.

--- Sky/app/src/main/java/com/example/kjy/sky/MainActivity.kt
+++ Sky/app/src/main/java/com/example/kjy/sky/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.kjy.sky
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
-
+import android.widget.Toast
 import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     var locationText : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // get reference to button
+        val searchButton = findViewById(R.id.imageButton) as ImageButton
+        // set on-click listener
+        searchButton.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "search loaction text", Toast.LENGTH_SHORT).show()
+        }
         setUI()
         runAPI()
     }
+
     fun setUI(){
         val myLocationTextView = findViewById(R.id.myLocationTV) as TextView
         locationText = myLocationTextView.text.toString()
